8825058c45d64700750f1ab5287f8b9f0596c584
SUNNY KUMAR <126163956+sunnykumar219@users.noreply.github.com>
2026-02-02 12:18:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('.'));
 
 const openai = new OpenAI({
   apiKey: "PASTE_YOUR_OPENAI_KEY_HERE",
